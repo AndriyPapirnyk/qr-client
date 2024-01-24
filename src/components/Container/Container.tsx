f3c5736558513914c89a9c1d39a414ae9fc4d42c
UserName <m492615892gmail.com>
2024-01-24 17:09:27 +0200

    add Header

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,35 +2,40 @@
 import './Container.scss';
 import { Link } from 'react-router-dom';
 import image from './Image/QRCode.png'
+import Header from '../Header/Header';
 const Container = () => {
     return (
+
         <div className='container'>
-            <div className='container__first-block'>
-                <div className="container__first-block_image" >
-                    <img src={image} alt="QR Code" />
+            <Header />
+            <div className="container__block-container">
+                <div className='container__first-block'>
+                    <div className="container__first-block_image" >
+                        <img src={image} alt="QR Code" />
+                    </div>
+
                 </div>
+                <div className='container__second-block'>
+                    <h3>Відскановано</h3>
+                    <div className='container__second-block_card-container'>
+                        <div className="container__second-block_card">
+                            <p>4</p>
+                        </div>
+                        <div className="container__second-block_card">
+                            <p>2</p>
+                        </div>
+                        <div className="container__second-block_card">
+                            <p>8</p>
+                        </div>
+                    </div>
 
-            </div>
-            <div className='container__second-block'>
-                <h3>Відскановано</h3>
-                <div className='container__second-block_card-container'>
-                    <div className="container__second-block_card">
-                        <p>4</p>
-                    </div>
-                    <div className="container__second-block_card">
-                        <p>2</p>
-                    </div>
-                    <div className="container__second-block_card">
-                        <p>8</p>
-                    </div>
+                    <Link to="/rating">
+                        <button className='container__second-block-btn'>
+                            Рейтинг
+                        </button>
+                    </Link>
+
                 </div>
-
-                <Link to="/rating">
-                    <button className='container__second-block-btn'>
-                        Рейтинг
-                    </button>
-                </Link>
-
             </div>
 
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+import './Header.scss';
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header__user'>
+                <p>Кл</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
